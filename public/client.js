84258362575c817cfe8b8e7eb49cf5ddd18a77b4
mit945 <mit945.mc@gmail.com>
2019-06-05 17:12:10 -0700

    datepicker

---
+++ public/client.js
@@ -0,0 +1,7 @@
+  console.log('load client.js')
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+  })
+
+  
